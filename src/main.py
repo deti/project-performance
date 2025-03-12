74c8122793d8e1b4a991484f6fa372ef9b911e9f
Denis Timofeev <detijazzz@gmail.com>
2025-03-12 17:14:28 +0400

    experiment on real data

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from settings import settings
-from jira_test import test_jira_connection
+from jira_data import get_jira_metrics
+from utils import make_file_name
 
 if __name__ == "__main__":
     print(f"JIRA_API_KEY: {settings.JIRA_API_KEY}")
@@ -9,11 +10,11 @@ if __name__ == "__main__":
     print(f"BITBUCKET_BASE_URL: {settings.BITBUCKET_BASE_URL}")
     print(f"BITBUCKET_PROJECT_KEY: {settings.BITBUCKET_PROJECT_KEY}")
 
-    test_jira_connection()
+    # test_jira_connection()
 
-    # start_date = "2025-03-01"
-    # jira_df = get_jira_metrics(start_date=start_date)
+    start_date = "2025-03-01"
+    jira_df = get_jira_metrics(start_date=start_date)
 
-    # # Export the DataFrame to Excel
-    # jira_df.to_excel(make_file_name(start_date, "jira_metrics.xlsx"), index=False)
-    # print("\nData exported to jira_metrics.xlsx")
+    # Export the DataFrame to Excel
+    jira_df.to_excel(make_file_name(start_date, "jira_metrics.xlsx"), index=False)
+    print("\nData exported to jira_metrics.xlsx")
